ed2b8c62a23ea1e9e85c9d7b39f742ee10bd7fe7
chrissx <chrissx@chrissx.de>
2021-03-12 19:02:10 +0100

    dsbuntis 3.1 and removed schttp dep

--- bin/accemus.dart
+++ bin/accemus.dart
@@ -1,11 +1,10 @@
 import 'package:dsbuntis/dsbuntis.dart';
-import 'package:schttp/schttp.dart';
 
 Future<int> main(List<String> args) async {
   if (args.length < 2) {
     print('Usage: accemus [username] [password]');
     return 1;
   }
-  print(Plan.plansToJson(await getAllSubs(args[0], args[1], ScHttpClient())));
+  print(Plan.plansToJson(await getAllSubs(args[0], args[1])));
   return 0;
 }
