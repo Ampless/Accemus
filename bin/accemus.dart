ee71b04774cd30cbbfb9ab9120047d86d191933f
chrissx <chrissx@chrissx.de>
2021-04-27 18:39:31 +0200

    dsbuntis 4.2

--- bin/accemus.dart
+++ bin/accemus.dart
@@ -2,16 +2,12 @@ import 'dart:io';
 
 import 'package:dsbuntis/dsbuntis.dart';
 
-Future<int> main(List<String> args) async {
+void main(List<String> args) async {
   if (args.length < 2) {
     stderr.writeln('Usage: accemus [username] [password]');
-    return 1;
+    exitCode = 1;
+    return;
   }
   final plans = await getAllSubs(args[0], args[1]);
-  if (plans == null) {
-    stderr.writeln('Please check your internet connection and credentials.');
-    return 2;
-  }
   print(Plan.plansToJsonString(plans));
-  return 0;
 }
