3d3106cf1046b2632a33bbdc6b2f50c1b60a54d5
pixel <chrissx@chrissx.de>
2021-08-24 15:50:44 +0200

    redid everything, it now has command line options

--- bin/accemus.dart
+++ bin/accemus.dart
@@ -1,13 +1,35 @@
 import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:dsbuntis/dsbuntis.dart';
 
-void main(List<String> args) async {
-  if (args.length < 2) {
-    stderr.writeln('Usage: accemus [username] [password]');
-    exitCode = 1;
+void main(List<String> argv) async {
+  final parser = ArgParser()
+    ..addOption('session', abbr: 's', help: 'skips the login')
+    ..addFlag('login-only',
+        abbr: 'l', help: 'only logs in and prints the session');
+  final args = parser.parse(argv);
+  if (args['login-only']) {
+    if (args.wasParsed('session') || args.rest.length != 2) {
+      stderr.writeln(parser.usage);
+      exitCode = 1;
+      return;
+    }
+    final session = await Session.login(args.rest[0], args.rest[1]);
+    print(session.token);
     return;
+  } else {
+    if (!args.wasParsed('session') && args.rest.length != 2) {
+      stderr.writeln(parser.usage);
+      exitCode = 1;
+      return;
+    }
+    final session = args.wasParsed('session')
+        ? Session.fromToken(args['session'])
+        : await Session.login(args.rest[0], args.rest[1]);
+    final json = await session.getTimetableJson();
+    for (final p in session.parsePlans(session.downloadPlans(json))) {
+      print(await p);
+    }
   }
-  final plans = await getAllSubs(args[0], args[1]);
-  print(Plan.plansToJsonString(plans));
 }
